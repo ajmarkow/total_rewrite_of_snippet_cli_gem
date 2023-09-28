bb8714b5ffbb26c6f35899ad765660fcc920e528
AJ Markow <alexanderjmarkow@gmail.com>
2023-09-28 09:05:26 -0500

    Add gitignore and other required files
    Add gemspec, gitignore, and spec file.

--- spec/snippet_cli_spec.rb
+++ spec/snippet_cli_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+require "./lib/snippet_cli"
+
 RSpec.describe SnippetCli do
-  it "has a version number" do
+  it "should have a version number." do
     expect(SnippetCli::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "should return the path of config file on mac." do
+    path_of_espanso_config = `echo $HOME`.chomp + "/Library/Application Support/espanso/match/base.yml"
+    expect(SnippetCli.config_path).to eq(path_of_espanso_config)
+
+    puts " TEST NOTE: In case of this test system, the path to config is  #{path_of_espanso_config}"
   end
 end
