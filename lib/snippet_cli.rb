b549bb7ab62fc33e5b53fb1ac5dcec19ef8090bc
AJ Markow <alexanderjmarkow@gmail.com>
2023-09-28 09:08:23 -0500

    Update github actions file. Commit Gemfile lock

--- lib/snippet_cli.rb
+++ lib/snippet_cli.rb
@@ -4,5 +4,9 @@ require_relative "snippet_cli/version"
 
 module SnippetCli
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.config_path
+    # Retrieves Espanso config path, appends folder name + main YAML filename to get file path to append snippets to.
+    `espanso path config`.chomp + "/match/base.yml"
+  end
 end
