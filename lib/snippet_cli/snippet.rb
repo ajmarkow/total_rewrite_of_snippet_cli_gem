7a8571bc6aea7ef8070968059938bc97b596c2ed
AJ Markow <alexanderjmarkow@gmail.com>
2023-09-30 13:19:48 -0500

    Scaffold out Snippet schema definition in dry-validation

--- lib/snippet_cli/snippet.rb
+++ lib/snippet_cli/snippet.rb
@@ -1,18 +1,34 @@
-require "shale/schema"
+require "dry-validation"
 
-class Snippet
-  def self.json_schema_to_shale
-    espanso_schema_json = File.open("espanso-schema-json/espanso-schema.json")
-    extracted_json = espanso_schema_json.read
-    schema = JSON.parse(extracted_json)
+class Snippet < Dry::Validation::Schema
+# Setting for dry-validation that makes explicit type definitions required
+  configure { config.type_specs = true }
 
-    puts Shale::Schema.from_json(
-      [schema],
-      root_name: "matches",
-      namespace_mapping: {
-        nil => "Api::Snippet", # default schema (without ID)
-        "http://bar.com" => "Api::Bar"
-      }
-    )
+  module TriggerTypes
+    include Dry::Types(default: :strict)
+    TypeOptions = String.enum("trigger", "regex")
+  end
+
+  module ReplaceTypes
+    include Dry::Types(default: :strict)
+    TypeOptions = String.enum("form", "replace", "image_path")
+  end
+
+  schema do
+    required(:trigger_type).value(TriggerTypes::TypeOptions).filled(:string)
+    required(:contents_of_trigger).value(:array, min_size?: 1)
+    required(:replace_type).value(ReplaceTypes::TypeOptions).filled(:string)
+    required(:contents_of_replace).filled(:string)
+    optional(:word).value(:bool?)
+    optional(:propagate_case).value(:bool?)
+    optional(:vars).hash
+  end
+
+  rule(word_key_absence: [:trigger_type, :word]) do |trigger_type, word|
+    trigger_type.value(eql?: "image_path").then(word.none?)
+  end
+
+  rule(propagate_case_key_absence: [:trigger_type, :propagate_case]) do |trigger_type, propagate_case|
+    trigger_type.value(eql?: "image_path").then(propagate_case.none?)
   end
 end
